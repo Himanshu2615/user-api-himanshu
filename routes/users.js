f561c03cee46b62d90f0f1930e707cca709c90ba
Himanshu Jha <himanshujha2615@gmail.com>
2026-03-13 12:09:44 +0530

    coo

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const express = require("express");//api
+const bcrypt = require("bcryptjs");//enc
+const jwt = require("jsonwebtoken");//
 const User = require("../models/User");
 const auth = require("../middleware/auth");
 
