ae8ada797b805807ebf3b487300789d7218d86c8
Himanshu Jha <himanshujha2615@gmail.com>
2026-03-13 16:11:27 +0530

    feat: prepare project for Vercel deployment

--- server.js
+++ server.js
@@ -19,8 +19,11 @@ app.get("/", (req, res) => {
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
